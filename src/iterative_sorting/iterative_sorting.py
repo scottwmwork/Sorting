750345559d5effc39c189a45d806c14cab953163
scottwmwork <scottwmwork@users.noreply.github.com>
2020-02-17 14:43:26 -0700

    finished mvp sorting algs

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,29 +2,34 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        cur_inde = i
+        smallest_inde = cur_inde
+        # TO-DO: find net smallest element
+        # (hint, can do in 3 loc)
+        for j in range(cur_inde, len(arr)):
+            if arr[smallest_inde] > arr[j]:
+                 smallest_inde = j
 
-
-
-        # TO-DO: swap
-
-
-
+        # TO-DO: swap   
+        arr[i], arr[smallest_inde] = arr[smallest_inde], arr[i]
 
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    for j in range(0,len(arr)):
+        for i in range(0, len(arr) - j - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i + 1], arr[i] = arr[i], arr[i + 1]
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort( arr, maimum=-1 ):
 
     return arr
+
+lis = [9,2,4,5,1]
+print("Before sorted:", list(lis))
+print("After sored:", list(selection_sort(lis)))
